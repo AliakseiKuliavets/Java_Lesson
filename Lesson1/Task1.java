bf10d87485cfe206769d2a29ec04757db58c3e7b
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-22 12:58:58 +0200

    Add files via upload

---
+++ Lesson1/Task1.java
@@ -0,0 +1,37 @@
+package org.telran;
+
+public class Task1 {
+    public static void main(String[] args) {
+//        int num ;
+//        num = 7;
+//        byte byteM = 66;
+//        float myFloat = 33.06f;
+
+//        System.out.println("Hello world! " + byteM);
+//        System.out.println("Hello World " + myFloat);
+//        System.out.println(num);
+//        run();
+
+
+         int num1 = 3;
+         int num2 = 10;
+         System.out.println(num1 + num2);
+         System.out.println(num1 - num2);
+         System.out.println(num1 * num2);
+         System.out.println(num2 / num1);
+         System.out.println(num2 % num1);
+
+         float result = (float)num2 / num1;
+         System.out.println(result);
+//          sum();
+    }
+//     static void run() {
+//        System.out.println("Runnnn!!!");
+//    }
+    static void sum () {
+         int num1 = 1;
+         int num2 = 2;
+         int result = num1 + num2;
+         System.out.println(result);
+    }
+}
