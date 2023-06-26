90e4cb0f093b3ef1f62d1caebb291007306d8756
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-26 12:53:19 +0200

    Add files via upload

---
+++ Lesson2/TestScanner.java
@@ -0,0 +1,41 @@
+package org.telran.Lesson2;
+
+import java.util.Scanner;
+
+public class TestScanner {
+    public static void main(String[] args) {
+        int myInt;
+        Scanner myScanner = new Scanner(System.in);
+
+
+        // integer
+        System.out.print("Write first number");
+        myInt = myScanner.nextInt();
+        System.out.println("Our number is: " + myInt);
+
+        System.out.println();
+        // boolean
+        System.out.print("true or false");
+        boolean myBool = myScanner.nextBoolean();
+        System.out.println("it`s: " + myBool);
+
+        System.out.println();
+        // String
+        Scanner myScannerString = new Scanner(System.in);
+        System.out.println("Write text");
+        String myString = myScannerString.nextLine();
+        System.out.print("Yor text: " + myString);
+
+        System.out.println();
+        // long
+        System.out.print("Long number");
+        long myLong = myScanner.nextLong();
+        System.out.println("Number i`ts: " + myLong);
+
+        System.out.println();
+        // float
+        System.out.print("Float number");
+        float myFloat = myScanner.nextFloat();
+        System.out.println("Number i`ts: " + myFloat);
+    }
+}
