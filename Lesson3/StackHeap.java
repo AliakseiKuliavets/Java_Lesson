bee2669cc4d5c572dc667d7e33583a9d2c7954a3
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-27 16:10:50 +0200

    Add files via upload

---
+++ Lesson3/StackHeap.java
@@ -0,0 +1,51 @@
+package org.telran.Lesson3;
+
+public class StackHeap {
+    public static void main(String[] args) {
+        int myInt1 = 10;
+        Test test1 = new Test();
+
+     /*   String myStr1 = new String("String");
+        String myStr2 = "I`m String";
+        myStr2 = "new line";
+        char c = 'r';
+        myStr2 = new String("new Line");
+        System.out.println(myStr2);
+        System.out.println(myStr2.length());
+        // длинна строки length()
+
+        System.out.println(myStr2.charAt(0));
+        // получить символ из строки по индексу charAt(index)
+
+        System.out.println(myStr2.charAt(myStr2.length()-1));
+        // получить последний символ из строки по индексу charAt(index)
+
+        System.out.println(myStr2.substring(0,3));
+        // отрезаем символ строку из текста substring(0,3) 0 это первый символ
+        // 3 это последний т.е. 2(последний эелемент)+1
+
+        String myStr3 = myStr2.substring(4); // откусывает от п=начального до конца
+        System.out.println(myStr3);*/
+
+
+        char r = 'r';
+        System.out.println(r);
+        System.out.println(Character.hashCode(r));
+
+
+
+
+
+/*        int myInt2 = myInt1;
+        System.out.println(myInt1 + "      "  +myInt2);
+        myInt2 = 20;
+        System.out.println(myInt1 + "      " + myInt2);
+
+        Test test2 = test1;
+        System.out.println(test1.myIntTest + "     " + test2.myIntTest);
+        test1.myIntTest = 20;
+        System.out.println(test1.myIntTest + "     " + test2.myIntTest);
+        test2.myIntTest = 99;
+        System.out.println(test1.myIntTest + "     " + test2.myIntTest);*/
+    }
+}
