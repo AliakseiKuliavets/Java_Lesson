2e416ea6d280fc142ee8c8bf9185d8892d00149d
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-30 12:19:03 +0200

    Add files via upload

---
+++ Lesson6Summary/NotMain.java
@@ -0,0 +1,12 @@
+package org.telran.Lesson6Summary;
+
+public class NotMain {
+
+    static int  size5 = 500;
+    int size1 = 100;
+    public int size2 = 200;
+    private int size3 = 300;
+
+    protected int size4 = 400;
+
+}
---
+++ Lesson6Summary/Practic/Dog.java
@@ -0,0 +1,22 @@
+package org.telran.Lesson6Summary.Practic;
+
+
+
+public class Dog {
+
+    public String name;
+    public int age;
+    public Dog() {
+        System.out.println("I`m constructor in Dog()");
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
