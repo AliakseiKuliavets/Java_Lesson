98adf0bf89faeba78c662db7168352a2f2474809
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-07-05 14:18:30 +0200

    Add files via upload

---
+++ Lesson9/IfElse.java
@@ -0,0 +1,21 @@
+package org.telran.Lesson9;
+
+import java.util.Random;
+
+public class IfElse {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(51) - 25;
+        System.out.println("Число до проверки: " + number);
+
+        if (number > 0) {
+            System.out.println("Температура в Цельсиях: " + number);
+        } else {
+            System.out.println("Увеличим температуру на 5 градусов: " + (number + 5));
+        }
+
+        System.out.println((number > 0) != (!(number > 0)));
+
+
+    }
+}
