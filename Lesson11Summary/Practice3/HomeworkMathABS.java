6544d921ff0ac4b5075d14ee7fb860184ee5227e
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-07-10 12:13:31 +0200

    Add files via upload

---
+++ Lesson11Summary/Practice3/HomeworkMathABS.java
@@ -0,0 +1,17 @@
+package org.telran.Lesson11Summary.Practice3;
+
+public class HomeworkMathABS {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 18;
+        int a = 10;
+        double b = Math.abs(a - num1);
+        double c = Math.abs(a - num2);
+        if (b > c) {
+            System.out.println(num2);
+        }
+        if (b < c) {
+            System.out.println(num1);
+        }
+    }
+}
