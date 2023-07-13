00789c5e2016aa81b6fe6ab30ecd3fa0f5167838
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-07-13 13:47:24 +0200

    Add files via upload

---
+++ Lesson15/practice/Pen.java
@@ -0,0 +1,20 @@
+package org.telran.Lesson15.practice;
+
+public class Pen {
+    private String color;
+
+    public Pen() {
+    }
+
+    public Pen(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
