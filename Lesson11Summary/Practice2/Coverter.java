f255636b4b1da87c684fcd3b6648e7984d72ba8b
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-08-11 12:45:09 +0200

    Delete Lesson11Summary directory

--- Lesson11Summary/Practice2/Coverter.java
+++
@@ -1,9 +0,0 @@
-package org.telran.Lesson11Summary.Practice2;
-
-public class Coverter {
-    public static double changeCelsiusToKelvin(double celsiusValue) {
-        return celsiusValue + 273.15;
-
-
-    }
-}
