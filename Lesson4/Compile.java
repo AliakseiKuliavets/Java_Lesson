cecf2b90ba87ebdcd69f0839ff6987ef2d2f43c0
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-08-17 09:59:01 +0200

    Delete Lesson4 directory

--- Lesson4/Compile.java
+++
@@ -1,7 +0,0 @@
-package org.telran.Lesson4;
-
-public class Compile {
-    public static void main(String[] args) {
-        System.out.println("I`m working");
-    }
-}
--- Lesson4/Counters.java
+++
@@ -1,8 +0,0 @@
-package org.telran.Lesson4;
-
-public class Counters {
-    public static void main(String[] args) {
-        int myInt = 25;
-        System.out.println(Long.toString(myInt, 2));
-    }
-}
